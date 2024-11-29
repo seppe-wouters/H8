ca49c5dc36f2b95aa17d5dd49676975696d8a55c
seppe.wouters <seppe.wouters@B17-09>
2024-11-29 15:13:17 +0100

    3

--- opgave10h8/Program.cs
+++ opgave10h8/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] getallen = new int[n];
+
+            Console.WriteLine("geef nu n aantal getallen: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("> ");
+                getallen[i] = Convert.ToInt32(Console.ReadLine());
+                
+            }
+
+            Console.WriteLine($"kleinste getal: {getallen.Min()}");
+            Console.WriteLine($"grootste getal: {getallen.Max()}");
+
+            for (int i = 0; i < getallen.Length; i++)
+            {
+                
+            }
         }
     }
 }
